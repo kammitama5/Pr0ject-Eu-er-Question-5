aef9d256bf91461bd14ef4bd132c420c53283469
kammitama5 <krystal.maughan@gmail.com>
2016-10-17 13:43:08 -0700

    1st commit

---
+++ _002.py
@@ -0,0 +1,8 @@
+# for all values of n
+# if n - 1 % == 0
+# go n - 2 .... until n - (n - 1) => recursive
+
+# set limit? eg values < 500 000
+# find value n between 1 to 20 where 
+# x (which represents value of x between 1 and 20)
+# recursively stepped through until x - (x - 1) % == 0
